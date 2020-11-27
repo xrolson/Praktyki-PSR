0aab7269fc3bf904ba4da0819246cc0484072b95
xrolson <kacper.rolbiecki@psr.gda.pl>
2020-11-27 14:27:14 +0100

    cwiczenia 27.11

--- src/Cwiczenia1/Main.java
+++ src/Cwiczenia1/Main.java
@@ -1,8 +1,15 @@
 package Cwiczenia1;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        int liczba1 = 1;
+
+        if (liczba1 < 10) {
+            liczba1++;
+            liczba1 = 1;
+            liczba1 = liczba1 % 2;
+
+        }
     }
-}
+
+}git
