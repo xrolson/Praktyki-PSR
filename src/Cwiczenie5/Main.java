65892e24ca29ad855ccc48c97bd6be55212ba473
xrolson <kacper.rolbiecki@psr.gda.pl>
2020-12-02 14:23:04 +0100

    lekcja 2.12

--- src/Cwiczenie5/Main.java
+++ src/Cwiczenie5/Main.java
@@ -1,7 +1,42 @@
 package Cwiczenie5;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void miejscazerowe( double delta, double a, double b ){
+
+        if(delta == 0) {
+            System.out.println("Jedno rozwiazanie funkcji");
+            double x0 = -b / (2 * a);
+            System.out.println("x0 wynosi:" + x0);
+        }else if (delta > 0){
+            double x1 = (-b - Math.sqrt(delta)) /(2*a);
+            double x2 = (-b + Math.sqrt(delta)) /(2*a);
+            System.out.println("x1 i x2 wynosza:" + x1 +" "+ x2 + " delta wynosi:" + delta);
+        } else {
+            System.out.println("Brak rozwiazan");
+        }
+    }
+
+
+    public static double obliczeniedelty(double a, double b, double c){
+       double delta = b*b-4*a*c;
+        return delta;
+    }
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczby: a,b,c");
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+
+        double delta = obliczeniedelty(a,b,c);
+        miejscazerowe(delta,a,b);
+
+        double delta1 = obliczeniedelty(a,b,c);
+        miejscazerowe(delta1,a -2,b -3);
 
     }
 }
--- src/cwiczenie4dom/Main.java
+++ src/cwiczenie4dom/Main.java
@@ -10,7 +10,7 @@ public class Main {
 //    public static int funkcja1 (int x){
 //        if ( x%2 == 0 ) return 0;
 //        else{
-//            return 0;
+//            return x;
 //        }
 //
 //    }
