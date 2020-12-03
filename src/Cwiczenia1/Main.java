a0844314f56d9fbe297a8d44680ba818b5ecfa7f
xrolson <kacper.rolbiecki@psr.gda.pl>
2020-12-03 16:24:04 +0100

    korekta 1 zadania

--- src/Cwiczenia1/Main.java
+++ src/Cwiczenia1/Main.java
@@ -2,10 +2,12 @@ package Cwiczenia1;
 
 public class Main {
     public static void main(String[] args) {
-       for(int liczba1 = 1; liczba1 > 11; liczba1++){
+       for(int liczba1 = 1; liczba1 <= 10; liczba1++){
 
-           if(liczba1% 2 ==0);
+           if(liczba1% 2 == 0)
+
            System.out.println(liczba1);
+
         }
 
     }
